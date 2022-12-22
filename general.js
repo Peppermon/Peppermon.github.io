2c5b0e0ab73f4bf9891dce9b13a5dc07fc0aa95b
Peppermon <121041432+Peppermon@users.noreply.github.com>
2022-12-22 16:04:50 -0500

    Update general.js
    Added support for changing the + and - icon.

--- general.js
+++ general.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
 	
 	let inputs = $(".collapse");
 	 
+	 // Each one of these will be ex span stadium-1
 	inputs.each(function (input) {
+		// On clicking each of the spans
 		 this.onclick = function() {
-			  $(this.nextElementSibling).toggle();
-			  updateLocalStorage();
+			 // Set next, which will be the ul stadium-1-list element
+			 var next = $(this.nextElementSibling);
+			 // Toggle stadium-1-list
+			 next.toggle();
+			 toggleSign(this);
+			 updateLocalStorage();
+			 // Send stadium-1 to the toggle function
 		 }
 	});
 	
@@ -22,10 +29,20 @@ $(document).ready(function() {
 		localStorage.setItem("list", listArray.toString());
 	}
 	
+	function toggleSign(item) {
+		var toggle = $(item).find(".toggle");
+		if ($(item.nextElementSibling).is(":visible"))
+			$(toggle).text("-");
+		else
+			$(toggle).text("+");
+		
+	}
+	
 	function setupDropdown() {
 		var listArray = localStorage.getItem("list").split(",");
 		listArray.forEach(item => {
 			var docItem = $("#" + item);
+			toggleSign(docItem);
 			docItem.toggle();
 		});
 	}
